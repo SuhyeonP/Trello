ac5e41474370a2e90c52e385f2d9243357c6a8e6
SuhyeonP <40413353+SuhyeonP@users.noreply.github.com>
2020-11-27 16:47:11 +0900

    before setting redux (#2)
    * before setting redux
    
    * login redux front setting success
    
    * Setting/back dev (#1)
    
    * Chore: 프리티어 적용
    
    Description
    통일된 코드 포맷을 유지하기 위해 적용
    
    * Chore: 속성 정리
    
    Description
    setting 지움 -> 현재 사용하지 않음
    prettier 설정 추가
    extends 설정 추가
    typescript 프로젝트 지정
    
    * Chore: package.json 정리
    
    Description
    morgan삭제 -> 현재 사용하지 않고, 나중에 사용한다면 debug 모듈을 생각하고 있음
    axios삭제 -> 프론트에서 사용하는 모듈
    prettier 모듈 추가
    eslint 관련된 모듈 추가
    
    * Chore: vscode 추가
    
    Description
    현재 사용중인 vscode 설정 파일 막음
    
    * Refactor: 사용하지 않는 모듈 제거
    
    Description
    morgan 모듈 코드 제거
    ,추가 -> prettier 적용됨
    
    * modify readme and cherry pick from develop
    
    * why
    
    * Docs: 기술스택 추가
    
    Description
    사용 언어 추가
    
    Co-authored-by: dimsssss <caruen@hanmail.net>

--- front/components/login.js
+++ front/components/login.js
@@ -1,13 +1,20 @@
 import React, { useCallback } from 'react';
 import { Form, Input } from 'antd';
+import { useDispatch } from 'react-redux';
 import useInput from '../exp/useInput';
+import { LOG_IN_REQUEST } from '../reducers/user';
 
 const LoginForm = () => {
   const [userId, onChangeId] = useInput('');
   const [userPassword, onChangePw] = useInput('');
+  const dispatch = useDispatch();
 
   const gotoLogin = useCallback(() => {
     console.log(userId, userPassword);
+    dispatch({
+      type: LOG_IN_REQUEST,
+      data: { userId, userPassword },
+    });
   }, [userId, userPassword]);
 
   return (
---
+++ front/sagas/index.js
@@ -0,0 +1,12 @@
+import { all, fork } from 'redux-saga/effects';
+import axios from 'axios';
+
+import userSaga from './user';
+
+axios.defaults.withCredentials = true;
+
+export default function* rootSaga() {
+  yield all([
+    fork(userSaga),
+  ]);
+}
