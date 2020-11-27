dfe1b73bd607491a16dee8da1f1da28418ceec91
badbeoti <59817244+badbeoti@users.noreply.github.com>
2020-11-27 18:44:48 +0900

    <style>: <Add css in board>

--- front/components/BoardTest.js
+++ front/components/BoardTest.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import { mainBoardSection } from '../css/mainboard';
+import { List } from 'antd';
+import {
+  boardSection,
+  boardHead,
+  boardBase,
+  listBox,
+  listData,
+} from '../css/mainboard';
 
 export default function BoardTest() {
   return (
     <>
-      <div css={mainBoardSection}>
-        head
-      </div>
-      <div>
-        base
-        <div>lists</div>
+      <div css={boardSection}>
+        <div css={boardHead}>head</div>
+        <div css={boardBase}>
+          <List
+            grid={{ gutter: 8, column: 4 }}
+            dataSource={listData}
+            renderItem={(item) => (
+              <List.Item>
+                <div css={listBox} title={item.title}>
+                  Card content
+                </div>
+              </List.Item>
+            )}
+          />
+        </div>
       </div>
     </>
   );
--- front/css/mainboard.js
+++ front/css/mainboard.js
@@ -1,11 +1,50 @@
 import { css } from '@emotion/react';
 
-const mainBoardSection = css`
- background-color: #0079bf;
- *{
-    margin:0;
-    padding:0;
- }
+const boardSection = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
-export { mainBoardSection };
+const boardHead = css`
+  width: 100%;
+  height: 8vh;
+  text-align: center;
+  background-color: #0079bf;
+  * {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+const boardBase = css`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding-top: 10px;
+  padding-left: 10px;
+`;
+
+const listBox = css`
+  background-color: #e8e8e8;
+  width: 300px;
+`;
+
+const listData = [
+  {
+    title: 'Title 1',
+  },
+  {
+    title: 'Title 2',
+  },
+  {
+    title: 'Title 3',
+  },
+  {
+    title: 'Title 4',
+  },
+];
+
+export { boardSection, boardHead, boardBase, listBox, listData };
