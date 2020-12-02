b198369030a1d22fe34d17334d9bcfa0abd5eba7
dimsssss <caruen@hanmail.net>
2020-12-02 21:36:37 +0900

    Feat: express 로드
    Description
    express 관련 모듈 불러오기

---
+++ back/app.js
@@ -0,0 +1,26 @@
+const createError = require('http-errors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+const indexRouter = require('./router/index');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
