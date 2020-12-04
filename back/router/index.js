da3e546bdaa3cb8d380479816e744265cf446ad1
dimsssss <caruen@hanmail.net>
2020-12-04 16:26:17 +0900

    Refactor: 루트 디렉토리 router 수정
    Description
    사용하지 않는 파라미터 삭제
    arrow 함수 사용
    한 번에 응답 코드를 알 수 있게 status 추가

--- back/router/index.js
+++ back/router/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', function (req, res) {
-  res.send('welcome');
+router.get('/', res => {
+  res.status(200).send('wlcm');
 });
 
 module.exports = router;
