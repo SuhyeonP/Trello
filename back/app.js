096a002cfd1cd2c6b109520ba016ebaf9c0d6b2c
dimsssss <caruen@hanmail.net>
2020-12-06 10:04:44 +0900

    Setting/remove typescript (#5)
    * Chore: 타입스크립트 파일삭제
    
    Description
    타입 스크립트 파일 삭제
    
    * Chore: 패키지 제거
    
    Description
    타입스크립트 관련 모듈 제거
    
    * Feat: 웹 서버 구동
    
    Description
    웹 서버 로드 코드
    
    * Feat: express 로드
    
    Description
    express 관련 모듈 불러오기
    
    * Feat: index 라우터
    
    Description
    루트 라우터 추가
    
    * Refactor: 애로우 함수로 변경
    
    Description
    함수 유효 범위를 통일
    
    * Chore: http-errors 추가
    
    Description
    production 환경에서도 사용하기 위해 추가
    
    * Refactor: extended 설정 값 수정
    
    Description:
    중첩된 object를 사용하게 해서
    application/x-www-form-urlencoded 처리가 가능해짐
    
    * Refactor: 중복 코드 삭제
    
    Description
    아래에 에러 관련된 코드가 있어서 중복된 내용 삭제
    
    * Feat: 실행 명령어 추가
    
    Description
    자동으로 반영된 코드를 적용해주는  nodemon 실행 명령어 추가
    
    * Chore: eslint, prettier 파일 추가
    
    Description
    일관성 있는 코딩컨벤션을 위해 추가
    
    * Refactor: 루트 디렉토리 router 수정
    
    Description
    사용하지 않는 파라미터 삭제
    arrow 함수 사용
    한 번에 응답 코드를 알 수 있게 status 추가
    
    * Hotfix: 파라미터 삭제로 인한 에러
    
    Description
    req를 지우면 req, res로 넘길 때 req가 res로 넘어오게 된다

---
+++ back/app.js
@@ -0,0 +1,22 @@
+const createError = require('http-errors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+const indexRouter = require('./router/index');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
