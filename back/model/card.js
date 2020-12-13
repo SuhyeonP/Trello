e460651e570082216baaff5685049a5ab1d784b7
dimsssss <caruen@hanmail.net>
2020-12-13 22:02:27 +0900

    Feat: 카드 모델 추가

---
+++ back/model/card.js
@@ -0,0 +1,49 @@
+import pkg from 'sequelize';
+
+const createCardInstance = async db => {
+  const { DataTypes } = pkg;
+
+  await db.define(
+    'card',
+    {
+      cardId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      cardListId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      cardTitle: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      order: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      background: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    },
+    {
+      freezeTableName: true,
+    },
+  );
+};
+
+export default createCardInstance;
