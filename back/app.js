623524977850dfd8ac2979c0f2daf1fb4ba0e69c
dimsssss <caruen@hanmail.net>
2020-12-13 22:05:29 +0900

    Feat: 라우터 적용
    Description
    카드 라우터 적용
    json 형태의 에러 처리로 수정
    sequelize 객체 넘겨주는 코드 추가

--- back/app.js
+++ back/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import indexRouter from './router/index.js';
+import cardRouter from './router/card.js';
 
 const app = express();
 
@@ -8,14 +9,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+  req.db = app.get('db');
+  next();
+});
+
 app.use('/', indexRouter);
+app.use('/card', cardRouter);
 
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(500).send({
+    err,
+    message: err.message,
+  });
 });
 
 export default app;
