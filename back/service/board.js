10d58248658714a53dc24c0745c8b6b84588121a
dimsssss <caruen@hanmail.net>
2020-12-14 18:00:47 +0900

    Feat: 보드 서비스 추가

---
+++ back/service/board.js
@@ -0,0 +1,23 @@
+const createDate = date => {
+  return `${date.getFullYear()}-${
+    date.getMonth() + 1
+  }-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+};
+
+const createBoard = async (db, boardData) => {
+  try {
+    const result = await db.board.create(boardData);
+    const returnValues = await result.get();
+
+    const newDateString = createDate(returnValues.createdAt);
+    returnValues.createdAt = newDateString;
+    returnValues.updatedAt = newDateString;
+
+    return returnValues;
+  } catch (e) {
+    console.error(e);
+    return e;
+  }
+};
+
+export default { createBoard };
