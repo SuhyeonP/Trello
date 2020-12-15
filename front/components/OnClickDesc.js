56dcd153976c59d6b729d9e833497d4777e4f175
Gyumong <rlaalsrb3559@naver.com>
2020-12-15 22:06:27 +0900

    모달창 만드는중 -descript 일단 css만,한파일에 작업해서나중에css파일따로빼내야댐

---
+++ front/components/OnClickDesc.js
@@ -0,0 +1,61 @@
+import React, { memo } from "react";
+import { OpenLinkSingle } from "../css/single";
+import { Input } from "antd";
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+const InputText = styled(Input.TextArea)`
+  height: 108px;
+  overflow: hidden;
+  padding: 8px 12px;
+  min-height: 108px;
+  margin-bottom: 8px;
+`;
+const buttonBar = css`
+  height: 32px;
+  font-size: 14px;
+  .save-button {
+    width: 52px;
+    height: 32px;
+    background-color: #5aac44;
+    box-sizing: border-box;
+    box-shadow: none;
+    border: none;
+    color: #fff;
+    margin-right: 4px;
+    line-height: 20px;
+    font-weight: 400;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 3px;
+    cursor: pointer;
+    padding: 6px 12px;
+    text-decoration: none;
+  }
+  .x-button {
+    height: 32px;
+    display: inline-block;
+    text-align: center;
+    text-decoration: none;
+    vertical-align: bottom;
+    width: 32px;
+  }
+`;
+
+const OnClickDesc = ({ onToggleDesc }) => {
+  return (
+    <div>
+      <InputText placeholder="Add a more detailed description..." />
+      <div css={buttonBar}>
+        <button type="submit" className="save-button">
+          Save
+        </button>
+        <button className="x-button" onClick={onToggleDesc}>
+          X
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default OnClickDesc;
--- front/pages/board/[bid].js
+++ front/pages/board/[bid].js
@@ -1,11 +1,13 @@
-import React, { useCallback, useState } from 'react';
-import { OpenLinkSingle } from '../../css/single';
-import useInput from '../../exp/useInput';
+import React, { useCallback, useState } from "react";
+import { OpenLinkSingle } from "../../css/single";
+import useInput from "../../exp/useInput";
+import { AlignLeftOutlined } from "@ant-design/icons";
+import OnClickDesc from "../../components/OnClickDesc";
 
 const SingleCard = () => {
-  const [changeTitle, onChangeTitle] = useInput('');
+  const [changeTitle, onChangeTitle] = useInput("");
   const [modifyTitle, setModifyTitle] = useState(false);
-
+  const [InputDesc, setInputDesc] = useState(false);
   const changeToModifyTitle = useCallback(() => {
     setModifyTitle(true);
   }, [changeTitle]);
@@ -16,20 +18,31 @@ const SingleCard = () => {
     }
   }, [modifyTitle]);
 
+  const onToggleDesc = useCallback(() => {
+    setInputDesc(!InputDesc);
+  }, [InputDesc]);
   return (
     <div css={OpenLinkSingle}>
       <div className="board-title">
         <span>✅</span>
         <div className="real-title">
           {!modifyTitle && <h2 onClick={changeToModifyTitle}>Title</h2>}
-          {modifyTitle && <input onChange={onChangeTitle} value={changeTitle} />}
+          {modifyTitle && (
+            <input onChange={onChangeTitle} value={changeTitle} />
+          )}
         </div>
         <div className="little-title">
           <p>in list : origin-list-name</p>
         </div>
-      </div>
-      <div className="" onClick={closeInputs}>
-        <p>zone</p>
+        <div className="real-title">
+          {/* <AlignLeftOutlined /> */}
+          <h2>Description</h2>
+          {InputDesc ? (
+            <OnClickDesc onToggleDesc={onToggleDesc} />
+          ) : (
+            <p onClick={onToggleDesc}>Add a more detailed description...</p>
+          )}
+        </div>
       </div>
     </div>
   );
