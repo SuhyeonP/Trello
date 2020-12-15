354e00785d23d4af43edc4b6175683369f5fbe3a
dimsssss <caruen@hanmail.net>
2020-12-15 15:20:19 +0900

    Feat: card list router 추가

---
+++ back/router/list.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import isValidListData from '../validation/list.js';
+import list from '../service/list.js';
+
+const router = express.Router();
+
+router.post('/', async (req, res, next) => {
+  if (isValidListData(req.body)) {
+    const result = await list.createList(req.db.models, req.body);
+    res.status(200).send(result);
+  }
+  next();
+});
+
+export default router;
