270c095eed4c93ad7825002e82f22747f1f29667
dimsssss <caruen@hanmail.net>
2020-12-16 00:31:35 +0900

    Refactor: 트랜잭션 추가
    Description
    두개의 테이블에 대한 동작이 있어서
    트랜잭션을 추가함

--- back/service/user.js
+++ back/service/user.js
@@ -1,36 +1,58 @@
 import bcrypt from 'bcrypt';
-import board from './board.js';
+
+// TODO create후에 반환되는 값들 중 날짜 값을 yyyy-mm-dd hh:mm:ss
+// 포맷으로 변경하는 코드가 중복되어서 사용되어짐. 모듈로 분리할것
+
+const createDate = date => {
+  return `${date.getFullYear()}-${
+    date.getMonth() + 1
+  }-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+};
 
 const createUser = async (db, signUpData) => {
-  const hashedPassword = await bcrypt.hash(signUpData.userPassword, 10);
-  const newUserData = await db.user.build({
-    userId: signUpData.userId,
-    userNickName: signUpData.userNickName,
-    userPassword: hashedPassword,
-  });
-
   try {
-    const hi = await newUserData.save();
-    const result = await db.board.build({
-      userId: Number(hi.dataValues.id),
-      createdAt: new Date(),
-      backgroundType: 0,
+    const { models } = db;
+    const hashedPassword = await bcrypt.hash(signUpData.userPassword, 10);
+    const successSignupData = await db.transaction(async t => {
+      const newUserData = await models.user.create(
+        {
+          userId: signUpData.userId,
+          userNickName: signUpData.userNickName,
+          userPassword: hashedPassword,
+        },
+        { transaction: t },
+      );
+
+      const { id } = newUserData.dataValues;
+      const result = await models.board.create(
+        {
+          userId: id,
+          boardTitle: 'todo',
+          backgroundType: 'color',
+          backgroundValue: 'blue',
+        },
+        { transaction: t },
+      );
+      const localTime = createDate(result.dataValues.createdAt);
+      result.dataValues.createdAt = localTime;
+      result.dataValues.updatedAt = localTime;
+      return result;
     });
-    result.save();
-    return hi;
+
+    return successSignupData;
   } catch (e) {
-    console.error(e);
+    console.log(e);
     return e;
   }
 };
 
 const findUser = async (db, userId) => {
-  const checkDuplicate = db.user.findOne({
+  const checkDuplicate = await db.user.findOne({
     where: {
       userId,
     },
   });
-  return checkDuplicate;
+  return checkDuplicate !== null;
 };
 
 const LoginUser = async (db, loginInfo) => {
