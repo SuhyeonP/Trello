6c2f66b4be9e9c2cb11b5d119076d321c2368597
ahah12k <ahah12k@gmail.com>
2020-12-15 21:33:39 +0900

    reback board

--- back/service/board.js
+++ back/service/board.js
@@ -4,9 +4,25 @@ const createDate = date => {
   }-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 };
 
+const createBoard = async (db, boardData) => {
+  try {
+    const result = await db.board.create(boardData);
+    const returnValues = await result.get();
+
+    const newDateString = createDate(returnValues.createdAt);
+    returnValues.createdAt = newDateString;
+    returnValues.updatedAt = newDateString;
+
+    return returnValues;
+  } catch (e) {
+    console.error(e);
+    return e;
+  }
+};
+
 const getBoard = async db => {
   const data = await db.board.findAll();
   return data;
 };
 
-export default { getBoard };
+export default { createBoard, getBoard };
