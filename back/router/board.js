89e98efae3d2260960c2bf3578bfe74f031bcf4f
ahah12k <ahah12k@gmail.com>
2020-12-22 15:15:52 +0900

    remove tc

--- back/router/board.js
+++ back/router/board.js
@@ -5,14 +5,9 @@ import board from '../service/board.js';
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-  try {
-    const { id } = req.user;
-    const result = await board.getBoard(req.db.models, id);
-    res.status(200).json(result);
-  } catch (err) {
-    console.error(err);
-    next(err);
-  }
+  const { id } = req.user;
+  const result = await board.getBoard(req.db.models, id);
+  res.status(200).json(result);
 });
 
 router.post('/', async (req, res, next) => {
@@ -24,23 +19,13 @@ router.post('/', async (req, res, next) => {
 });
 
 router.patch('/title', async (req, res, next) => {
-  try {
-    const result = await board.modifyTitle(req.db.models, req.body);
-    res.status(200).json(result);
-  } catch (err) {
-    console.error(err);
-    next(err);
-  }
+  const result = await board.modifyTitle(req.db.models, req.body);
+  res.status(200).json(result);
 });
 
 router.patch('/background', async (req, res, next) => {
-  try {
-    const result = await board.modifyBG(req.db.models, req.body);
-    res.status(200).json(result);
-  } catch (err) {
-    console.error(err);
-    next(err);
-  }
+  const result = await board.modifyBG(req.db.models, req.body);
+  res.status(200).json(result);
 });
 
 export default router;
