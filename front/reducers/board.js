942b8ebf056a0116866316a41c717f85ed2c2451
Gyumong <rlaalsrb3559@naver.com>
2020-12-25 12:32:59 +0900

    redux immer와 함수 빼서 코드 단축

--- front/reducers/board.js
+++ front/reducers/board.js
@@ -1,5 +1,6 @@
-import produce from 'immer';
-import { createReducer } from '@reduxjs/toolkit';
+/** @format */
+
+import createReducer from "./createReducer";
 
 export const initialState = {
   mainLists: [],
@@ -24,159 +25,159 @@ export const initialState = {
   deleteCardError: null,
 };
 
-export const LOAD_MAIN_REQUEST = 'LOAD_MAIN_REQUEST';
-export const LOAD_MAIN_SUCCESS = 'LOAD_MAIN_SUCCESS';
-export const LOAD_MAIN_FAILURE = 'LOAD_MAIN_FAILURE';
+export const LOAD_MAIN_REQUEST = "LOAD_MAIN_REQUEST";
+export const LOAD_MAIN_SUCCESS = "LOAD_MAIN_SUCCESS";
+export const LOAD_MAIN_FAILURE = "LOAD_MAIN_FAILURE";
 
-export const LOAD_CARD_REQUEST = 'LOAD_CARD_REQUEST';
-export const LOAD_CARD_SUCCESS = 'LOAD_CARD_SUCCESS';
-export const LOAD_CARD_FAILURE = 'LOAD_CARD_FAILURE';
+export const LOAD_CARD_REQUEST = "LOAD_CARD_REQUEST";
+export const LOAD_CARD_SUCCESS = "LOAD_CARD_SUCCESS";
+export const LOAD_CARD_FAILURE = "LOAD_CARD_FAILURE";
 
-export const ADD_LIST_REQUEST = 'ADD_LIST_REQUEST';
-export const ADD_LIST_SUCCESS = 'ADD_LIST_SUCCESS';
-export const ADD_LIST_FAILURE = 'ADD_LIST_FAILURE';
+export const ADD_LIST_REQUEST = "ADD_LIST_REQUEST";
+export const ADD_LIST_SUCCESS = "ADD_LIST_SUCCESS";
+export const ADD_LIST_FAILURE = "ADD_LIST_FAILURE";
 
-export const ADD_CARD_REQUEST = 'ADD_CARD_REQUEST';
-export const ADD_CARD_SUCCESS = 'ADD_CARD_SUCCESS';
-export const ADD_CARD_FAILURE = 'ADD_CARD_FAILURE';
+export const ADD_CARD_REQUEST = "ADD_CARD_REQUEST";
+export const ADD_CARD_SUCCESS = "ADD_CARD_SUCCESS";
+export const ADD_CARD_FAILURE = "ADD_CARD_FAILURE";
 
-export const MODIFY_BOARD_TT_REQUEST = 'MODIFY_BOARD_TT_REQUEST';
-export const MODIFY_BOARD_TT_SUCCESS = 'MODIFY_BOARD_TT_SUCCESS';
-export const MODIFY_BOARD_TT_FAILURE = 'MODIFY_BOARD_TT_FAILURE';
+export const MODIFY_BOARD_TT_REQUEST = "MODIFY_BOARD_TT_REQUEST";
+export const MODIFY_BOARD_TT_SUCCESS = "MODIFY_BOARD_TT_SUCCESS";
+export const MODIFY_BOARD_TT_FAILURE = "MODIFY_BOARD_TT_FAILURE";
 
-export const MODIFY_BOARD_REQUEST = 'MODIFY_BOARD_REQUEST';
-export const MODIFY_BOARD_SUCCESS = 'MODIFY_BOARD_SUCCESS';
-export const MODIFY_BOARD_FAILURE = 'MODIFY_BOARD_FAILURE';
+export const MODIFY_BOARD_REQUEST = "MODIFY_BOARD_REQUEST";
+export const MODIFY_BOARD_SUCCESS = "MODIFY_BOARD_SUCCESS";
+export const MODIFY_BOARD_FAILURE = "MODIFY_BOARD_FAILURE";
 
-export const MODIFY_LIST_REQUEST = 'MODIFY_LIST_REQUEST';
-export const MODIFY_LIST_SUCCESS = 'MODIFY_LIST_SUCCESS';
-export const MODIFY_LIST_FAILURE = 'MODIFY_LIST_FAILURE';
+export const MODIFY_LIST_REQUEST = "MODIFY_LIST_REQUEST";
+export const MODIFY_LIST_SUCCESS = "MODIFY_LIST_SUCCESS";
+export const MODIFY_LIST_FAILURE = "MODIFY_LIST_FAILURE";
 
-export const MODIFY_CARD_REQUEST = 'MODIFY_CARD_REQUEST';
-export const MODIFY_CARD_SUCCESS = 'MODIFY_CARD_SUCCESS';
-export const MODIFY_CARD_FAILURE = 'MODIFY_CARD_FAILURE';
+export const MODIFY_CARD_REQUEST = "MODIFY_CARD_REQUEST";
+export const MODIFY_CARD_SUCCESS = "MODIFY_CARD_SUCCESS";
+export const MODIFY_CARD_FAILURE = "MODIFY_CARD_FAILURE";
 
-export const DELETE_CARD_REQUEST = 'DELETE_CARD_REQUEST';
-export const DELETE_CARD_SUCCESS = 'DELETE_CARD_SUCCESS';
-export const DELETE_CARD_FAILURE = 'DELETE_CARD_FAILURE';
+export const DELETE_CARD_REQUEST = "DELETE_CARD_REQUEST";
+export const DELETE_CARD_SUCCESS = "DELETE_CARD_SUCCESS";
+export const DELETE_CARD_FAILURE = "DELETE_CARD_FAILURE";
 
 export default createReducer(initialState, {
-  [LOAD_MAIN_REQUEST]: (state) => produce(state, (draft) => {
-    draft.loadingBoards = true;
-    draft.loadBoardError = null;
-    draft.loadedBoards = false;
-  }),
-  [LOAD_MAIN_SUCCESS]: (state, action) => produce(state, (draft) => {
-    draft.loadingBoards = false;
-    draft.mainLists = action.data;
-    draft.loadedBoards = true;
-  }),
-  [LOAD_MAIN_FAILURE]: (state, action) => produce(state, (draft) => {
-    draft.loadingBoards = false;
-    draft.loadBoardError = action.error;
-  }),
-  [LOAD_CARD_REQUEST]: (state) => produce(state, (draft) => {
-    draft.loadingCard = true;
-    draft.loadCardError = null;
-    draft.loadedCard = false;
-  }),
-  [LOAD_CARD_SUCCESS]: (state, action) => produce(state, (draft) => {
-    draft.loadingCard = false;
-    draft.loadedCard = true;
-    draft.cardModal = action.data;
-  }),
-  [LOAD_CARD_FAILURE]: (state, action) => produce(state, (draft) => {
-    draft.loadingCard = false;
-    draft.loadCardError = action.error;
-  }),
-  [ADD_LIST_REQUEST]: (state) => produce(state, (draft) => {
-    draft.addListLoading = true;
-    draft.addListError = null;
-    draft.logOutDone = false;
-  }),
-  [ADD_LIST_SUCCESS]: (state, action) => produce(state, (draft) => {
-    draft.addListLoading = false;
-    draft.addListDone = true;
-    draft.mainLists = action.data;
-  }),
-  [ADD_LIST_FAILURE]: (state, action) => produce(state, (draft) => {
-    draft.addListLoading = false;
-    draft.addListError = action.error;
-  }),
-  [ADD_CARD_REQUEST]: (state) => produce(state, (draft) => {
-    draft.addCardLoading = true;
-    draft.addCardError = null;
-    draft.addCardDone = false;
-  }),
-  [ADD_CARD_SUCCESS]: (state, action) => produce(state, (draft) => {
-    draft.addCardLoading = false;
-    draft.addCardDone = true;
-    draft.mainLists = action.data;
-  }),
-  [ADD_CARD_FAILURE]: (state, action) => produce(state, (draft) => {
-    draft.addCardLoading = false;
-    draft.addCardError = action.error;
-  }),
-  [MODIFY_BOARD_TT_REQUEST]: (state) => produce(state, (draft) => {
-    draft.modifyTextLoading = true;
-    draft.modifyTextError = null;
-  }),
-  [MODIFY_BOARD_REQUEST]: (state) => produce(state, (draft) => {
-    draft.modifyTextLoading = true;
-    draft.modifyTextError = null;
-  }),
-  [MODIFY_LIST_REQUEST]: (state) => produce(state, (draft) => {
-    draft.modifyTextLoading = true;
-    draft.modifyTextError = null;
-  }),
-  [MODIFY_CARD_REQUEST]: (state) => produce(state, (draft) => {
-    draft.modifyTextLoading = true;
-    draft.modifyTextError = null;
-  }),
-  [MODIFY_BOARD_TT_SUCCESS]: (state, action) => produce(state, (draft) => {
-    draft.modifyTextDone = true;
-    draft.modifyTextLoading = false;
-    draft.mainLists = action.data;
-  }),
-  [MODIFY_BOARD_SUCCESS]: (state, action) => produce(state, (draft) => {
-    draft.modifyTextDone = true;
-    draft.modifyTextLoading = false;
-    draft.mainLists = action.data;
-  }),
-  [MODIFY_LIST_SUCCESS]: (state) => produce(state, (draft) => {
-    draft.modifyTextDone = true;
-    draft.modifyTextLoading = false;
-  }),
-  [MODIFY_CARD_SUCCESS]: (state) => produce(state, (draft) => {
-    draft.modifyTextDone = true;
-    draft.modifyTextLoading = false;
-  }),
-  [MODIFY_BOARD_TT_FAILURE]: (state, action) => produce(state, (draft) => {
-    draft.modifyTextDone = false;
-    draft.modifyTextError = action.error;
-  }),
-  [MODIFY_BOARD_FAILURE]: (state, action) => produce(state, (draft) => {
-    draft.modifyTextDone = false;
-    draft.modifyTextError = action.error;
-  }),
-  [MODIFY_LIST_FAILURE]: (state, action) => produce(state, (draft) => {
-    draft.modifyTextDone = false;
-    draft.modifyTextError = action.error;
-  }),
-  [MODIFY_CARD_FAILURE]: (state, action) => produce(state, (draft) => {
-    draft.modifyTextDone = false;
-    draft.modifyTextError = action.error;
-  }),
-  [DELETE_CARD_REQUEST]: (state) => produce(state, (draft) => {
-    draft.deleteCardLoading = true;
-    draft.deleteCardError = null;
-  }),
-  [DELETE_CARD_SUCCESS]: (state) => produce(state, (draft) => {
-    draft.deleteCardLoading = false;
-    draft.deleteCardDone = true;
-  }),
-  [DELETE_CARD_FAILURE]: (state, action) => produce(state, (draft) => {
-    draft.deleteCardLoading = false;
-    draft.deleteCardError = action.error;
-  }),
+  [LOAD_MAIN_REQUEST]: (state) => {
+    state.loadingBoards = true;
+    state.loadBoardError = null;
+    state.loadedBoards = false;
+  },
+  [LOAD_MAIN_SUCCESS]: (state, action) => {
+    state.loadingBoards = false;
+    state.mainLists = action.data;
+    state.loadedBoards = true;
+  },
+  [LOAD_MAIN_FAILURE]: (state, action) => {
+    state.loadingBoards = false;
+    state.loadBoardError = action.error;
+  },
+  [LOAD_CARD_REQUEST]: (state) => {
+    state.loadingCard = true;
+    state.loadCardError = null;
+    state.loadedCard = false;
+  },
+  [LOAD_CARD_SUCCESS]: (state, action) => {
+    state.loadingCard = false;
+    state.loadedCard = true;
+    state.cardModal = action.data;
+  },
+  [LOAD_CARD_FAILURE]: (state, action) => {
+    state.loadingCard = false;
+    state.loadCardError = action.error;
+  },
+  [ADD_LIST_REQUEST]: (state) => {
+    state.addListLoading = true;
+    state.addListError = null;
+    state.logOutDone = false;
+  },
+  [ADD_LIST_SUCCESS]: (state, action) => {
+    state.addListLoading = false;
+    state.addListDone = true;
+    state.mainLists = action.data;
+  },
+  [ADD_LIST_FAILURE]: (state, action) => {
+    state.addListLoading = false;
+    state.addListError = action.error;
+  },
+  [ADD_CARD_REQUEST]: (state) => {
+    state.addCardLoading = true;
+    state.addCardError = null;
+    state.addCardDone = false;
+  },
+  [ADD_CARD_SUCCESS]: (state, action) => {
+    state.addCardLoading = false;
+    state.addCardDone = true;
+    state.mainLists = action.data;
+  },
+  [ADD_CARD_FAILURE]: (state, action) => {
+    state.addCardLoading = false;
+    state.addCardError = action.error;
+  },
+  [MODIFY_BOARD_TT_REQUEST]: (state) => {
+    state.modifyTextLoading = true;
+    state.modifyTextError = null;
+  },
+  [MODIFY_BOARD_REQUEST]: (state) => {
+    state.modifyTextLoading = true;
+    state.modifyTextError = null;
+  },
+  [MODIFY_LIST_REQUEST]: (state) => {
+    state.modifyTextLoading = true;
+    state.modifyTextError = null;
+  },
+  [MODIFY_CARD_REQUEST]: (state) => {
+    state.modifyTextLoading = true;
+    state.modifyTextError = null;
+  },
+  [MODIFY_BOARD_TT_SUCCESS]: (state, action) => {
+    state.modifyTextDone = true;
+    state.modifyTextLoading = false;
+    state.mainLists = action.data;
+  },
+  [MODIFY_BOARD_SUCCESS]: (state, action) => {
+    state.modifyTextDone = true;
+    state.modifyTextLoading = false;
+    state.mainLists = action.data;
+  },
+  [MODIFY_LIST_SUCCESS]: (state) => {
+    state.modifyTextDone = true;
+    state.modifyTextLoading = false;
+  },
+  [MODIFY_CARD_SUCCESS]: (state) => {
+    state.modifyTextDone = true;
+    state.modifyTextLoading = false;
+  },
+  [MODIFY_BOARD_TT_FAILURE]: (state, action) => {
+    state.modifyTextDone = false;
+    state.modifyTextError = action.error;
+  },
+  [MODIFY_BOARD_FAILURE]: (state, action) => {
+    state.modifyTextDone = false;
+    state.modifyTextError = action.error;
+  },
+  [MODIFY_LIST_FAILURE]: (state, action) => {
+    state.modifyTextDone = false;
+    state.modifyTextError = action.error;
+  },
+  [MODIFY_CARD_FAILURE]: (state, action) => {
+    state.modifyTextDone = false;
+    state.modifyTextError = action.error;
+  },
+  [DELETE_CARD_REQUEST]: (state) => {
+    state.deleteCardLoading = true;
+    state.deleteCardError = null;
+  },
+  [DELETE_CARD_SUCCESS]: (state) => {
+    state.deleteCardLoading = false;
+    state.deleteCardDone = true;
+  },
+  [DELETE_CARD_FAILURE]: (state, action) => {
+    state.deleteCardLoading = false;
+    state.deleteCardError = action.error;
+  },
 });
--- front/reducers/user.js
+++ front/reducers/user.js
@@ -1,5 +1,6 @@
-import produce from 'immer';
-import { createReducer } from '@reduxjs/toolkit';
+/** @format */
+
+import createReducer from "./createReducer";
 
 export const initialState = {
   loadUserLoading: false,
@@ -17,76 +18,76 @@ export const initialState = {
   me: null,
 };
 
-export const LOG_IN_REQUEST = 'LOG_IN_REQUEST';
-export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS';
-export const LOG_IN_FAILURE = 'LOG_IN_FAILURE';
+export const LOG_IN_REQUEST = "LOG_IN_REQUEST";
+export const LOG_IN_SUCCESS = "LOG_IN_SUCCESS";
+export const LOG_IN_FAILURE = "LOG_IN_FAILURE";
 
-export const LOG_OUT_REQUEST = 'LOG_OUT_REQUEST';
-export const LOG_OUT_SUCCESS = 'LOG_OUT_SUCCESS';
-export const LOG_OUT_FAILURE = 'LOG_OUT_FAILURE';
+export const LOG_OUT_REQUEST = "LOG_OUT_REQUEST";
+export const LOG_OUT_SUCCESS = "LOG_OUT_SUCCESS";
+export const LOG_OUT_FAILURE = "LOG_OUT_FAILURE";
 
-export const SIGN_UP_REQUEST = 'SIGN_UP_REQUEST';
-export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
-export const SIGN_UP_FAILURE = 'SIGN_UP_FAILURE';
+export const SIGN_UP_REQUEST = "SIGN_UP_REQUEST";
+export const SIGN_UP_SUCCESS = "SIGN_UP_SUCCESS";
+export const SIGN_UP_FAILURE = "SIGN_UP_FAILURE";
 
-export const RELOAD_USER_REQUEST = 'RELOAD_USER_REQUEST';
-export const RELOAD_USER_SUCCESS = 'RELOAD_USER_SUCCESS';
-export const RELOAD_USER_FAILURE = 'RELOAD_USER_FAILURE';
+export const RELOAD_USER_REQUEST = "RELOAD_USER_REQUEST";
+export const RELOAD_USER_SUCCESS = "RELOAD_USER_SUCCESS";
+export const RELOAD_USER_FAILURE = "RELOAD_USER_FAILURE";
 
 export default createReducer(initialState, {
-  [LOG_IN_REQUEST]: (state) => produce(state, (draft) => {
-    draft.logInLoading = true;
-    draft.logInError = null;
-    draft.logInDone = false;
-  }),
-  [LOG_IN_SUCCESS]: (state, action) => produce(state, (draft) => {
-    draft.logInLoading = false;
-    draft.me = action.data;
-    draft.logInDone = true;
-  }),
-  [LOG_IN_FAILURE]: (state, action) => produce(state, (draft) => {
-    draft.logInLoading = false;
-    draft.logInError = action.error;
-  }),
-  [LOG_OUT_REQUEST]: (state) => produce(state, (draft) => {
-    draft.logOutLoading = true;
-    draft.logOutError = null;
-    draft.logOutDone = false;
-  }),
-  [LOG_OUT_SUCCESS]: (state) => produce(state, (draft) => {
-    draft.logOutLoading = false;
-    draft.logOutDone = true;
-    draft.me = null;
-  }),
-  [LOG_OUT_FAILURE]: (state, action) => produce(state, (draft) => {
-    draft.logOutLoading = false;
-    draft.logOutError = action.error;
-  }),
-  [RELOAD_USER_REQUEST]: (state) => produce(state, (draft) => {
-    draft.loadUserLoading = true;
-    draft.loadUserError = null;
-    draft.loadUserDone = false;
-  }),
-  [RELOAD_USER_SUCCESS]: (state, action) => produce(state, (draft) => {
-    draft.loadUserLoading = false;
-    draft.me = action.data;
-    draft.loadUserDone = true;
-  }),
-  [RELOAD_USER_FAILURE]: (state, action) => produce(state, (draft) => {
-    draft.loadUserLoading = false;
-    draft.loadUserError = action.error;
-  }),
-  [SIGN_UP_REQUEST]: (state) => produce(state, (draft) => {
-    draft.signUpLoading = true;
-    draft.signUpError = null;
-    draft.signUpDone = false;
-  }),
-  [SIGN_UP_SUCCESS]: (state) => produce(state, (draft) => {
-    draft.signUpLoading = false;
-    draft.signUpDone = true;
-  }),
-  [SIGN_UP_FAILURE]: (state, action) => produce(state, (draft) => {
-    draft.signUpLoading = false;
-    draft.signUpError = action.error;
-  }),
+  [LOG_IN_REQUEST]: (state) => {
+    state.logInLoading = true;
+    state.logInError = null;
+    state.logInDone = false;
+  },
+  [LOG_IN_SUCCESS]: (state, action) => {
+    state.logInLoading = false;
+    state.me = action.data;
+    state.logInDone = true;
+  },
+  [LOG_IN_FAILURE]: (state, action) => {
+    state.logInLoading = false;
+    state.logInError = action.error;
+  },
+  [LOG_OUT_REQUEST]: (state) => {
+    state.logOutLoading = true;
+    state.logOutError = null;
+    state.logOutDone = false;
+  },
+  [LOG_OUT_SUCCESS]: (state) => {
+    state.logOutLoading = false;
+    state.logOutDone = true;
+    state.me = null;
+  },
+  [LOG_OUT_FAILURE]: (state, action) => {
+    state.logOutLoading = false;
+    state.logOutError = action.error;
+  },
+  [RELOAD_USER_REQUEST]: (state) => {
+    state.loadUserLoading = true;
+    state.loadUserError = null;
+    state.loadUserDone = false;
+  },
+  [RELOAD_USER_SUCCESS]: (state, action) => {
+    state.loadUserLoading = false;
+    state.me = action.data;
+    state.loadUserDone = true;
+  },
+  [RELOAD_USER_FAILURE]: (state, action) => {
+    state.loadUserLoading = false;
+    state.loadUserError = action.error;
+  },
+  [SIGN_UP_REQUEST]: (state) => {
+    state.signUpLoading = true;
+    state.signUpError = null;
+    state.signUpDone = false;
+  },
+  [SIGN_UP_SUCCESS]: (state) => {
+    state.signUpLoading = false;
+    state.signUpDone = true;
+  },
+  [SIGN_UP_FAILURE]: (state, action) => {
+    state.signUpLoading = false;
+    state.signUpError = action.error;
+  },
 });
